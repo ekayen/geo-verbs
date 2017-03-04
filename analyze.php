c8a0c47898b9d0f1d9aeb5233935ad417143ea8d
ekayen <ekayen@gmail.com>
2017-03-03 16:26:03 -0800

    Updated filepaths for xfst files

--- analyze.php
+++ analyze.php
@@ -6,29 +6,32 @@
 
  <?php 
 	$iv = htmlspecialchars($_GET["inVerb"]);
-	exec("C:\\Users\\ekaye\\Documents\\bin\\xfst -e \"read lexc < C:\\Users\\ekaye\\Documents\\bin\\geo-verbs-I.txt\" -e \"up $iv\" exit", $output, $rv);
+	exec("C:\\Users\\ekaye\\Documents\\GitHub\\geo-verbs\\xfst -e \"read lexc < C:\\Users\\ekaye\\Documents\\GitHub\\geo-verbs\\geo-verbs-I.txt\" -e \"up $iv\" exit", $output, $rv);
 	
-
-	//$x = var_dump($output);
-	//$y = var_dump($rv);
-
+/*
+	$x = var_dump($output);
+	$y = var_dump($rv);
+*/
 	$w = $output;
 
-	//echo $x." noah ".sizeof($output)." liz ".$rv." bleh".$y;
+	//echo $x." noah ".sizeof($output)." liz ".$rv." bucolic ".$y;
 ?> 
 
 <p>
 
 <strong>
 		<?php
-			//for($i = 0; $i < sizeof($w); $i++){
-			//	echo $i.". ".$w[$i]."<br />";
-			//}
+/*			
+			for($i = 0; $i < sizeof($w); $i++){
+				echo $i.". ".$w[$i]."<br />";
+			}
+*/			
 			if($w[sizeof($w)-1]=="???"){
 				echo "No analysis found for ".$iv;
 			} else {
 				echo "Analysis of ".$iv.": ".$w[sizeof($w)-1];
 			}
+			
 		?>
 
 </strong>
